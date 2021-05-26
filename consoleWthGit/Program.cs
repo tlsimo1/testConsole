5bdafe97fed7575883a28d1edba7b837284a43f3
simo <tl_simao@hotmail.com>
2021-05-26 14:56:35 +0000

    first change

---
+++ consoleWthGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleWthGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("firt test");
+            Console.WriteLine("firt test");
+        }
+    }
+}
